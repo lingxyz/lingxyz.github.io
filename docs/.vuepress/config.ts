647ff3ec71e0c7e20bcb47789de64cbc6f1ac26f
Ling Z <771152260@qq.com>
2023-03-21 11:08:32 +0800

    feat: aboutme页面改造成插件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -13,6 +13,9 @@ export default defineUserConfig({
     ['link', { rel: 'stylesheet', href: '//cdn.bootcss.com/animate.css/3.5.2/animate.min.css' }],
     ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/font_987629_b1cqvuf9roo.css' }]
   ],
+  plugins: [
+    require('./plugins/aboutPage/index.ts')
+  ],
   // 默认theme配置：https://github.com/vuepress/vuepress-next/tree/main/packages/%40vuepress/theme-default
   theme: defaultTheme({
     logo: '/images/photo.webp',
---
+++ docs/.vuepress/plugins/aboutPage/index.ts
@@ -0,0 +1,33 @@
+// 关于页面（省去md）
+// todo: 做成简历，加密码
+
+import { createPage } from '@vuepress/core'
+import { getDirname, path } from '@vuepress/utils'
+import jsyaml from 'js-yaml'
+import  fs from  'fs';
+const aboutData = fs.readFileSync(path.resolve(__dirname, './data.yaml'), 'utf8')
+
+const createAboutPage = (options, app) => {
+  return {
+    name: 'vuepress-plugin-createAboutPage',
+    async onInitialized(app) {
+      // if aboutpage doesn't exist
+      if (app.pages.every((page) => page.path !== '/aboutme')) {
+        // 页面数据准备
+        const aboutmeDB = jsyaml.load(aboutData)
+        // 创建一个关于页
+        const aboutpage = await createPage(app, {
+          path: '/aboutme',
+          // 设置 frontmatter
+          frontmatter: aboutmeDB,
+          // 设置 markdown 内容
+          // content: ``
+        })
+        // 把它添加到 `app.pages`
+        app.pages.push(aboutpage)
+      }
+    },
+  }
+}
+
+module.exports = createAboutPage
